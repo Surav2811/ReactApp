4c3d9d1c3ad1f9c7a1c96f092a4dacf11e9d5103
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-04-24 15:33:19 +0530

    Update Gallery.js
    Added new image

--- first-react-todo-app/Gallery.js
+++ first-react-todo-app/Gallery.js
@@ -1,8 +1,8 @@
 function Profile() {
   return (
     <img
-      src="https://i.imgur.com/QIrZWGIs.jpg"
-      alt="Alan L. Hart"
+      src="https://www.google.com/imgres?imgurl=https%3A%2F%2F1.bp.blogspot.com%2F-kK7Fxm7U9o0%2FYN0bSIwSLvI%2FAAAAAAAACFk%2FaF4EI7XU_ashruTzTIpifBfNzb4thUivACLcBGAsYHQ%2Fs1280%2F222.jpg&tbnid=p0ioo7EjOLaHfM&vet=12ahUKEwiRjNrSocL-AhVP-XMBHaetAV4QMygOegUIARCDAg..i&imgrefurl=https%3A%2F%2Fwww.argallery.in%2F2021%2F06%2Fimages-of-nature-beauty%2520.html&docid=gIh7exlZdTaWnM&w=1280&h=720&q=images&ved=2ahUKEwiRjNrSocL-AhVP-XMBHaetAV4QMygOegUIARCDAg"
+      alt="Something something"
     />
   );
 }
