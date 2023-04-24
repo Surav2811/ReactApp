426da3351098d85f52b8ccfbe920b174e042f971
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-04-24 16:21:45 +0530

    Add files via upload

---
+++ Tutorial/SelectiveRendering/app.js
@@ -0,0 +1,19 @@
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+export default function List() {
+  const listItems = people.map(person =>
+    <li key={person.id}>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}</b>
+          {' ' + person.profession + ' '}
+          known for {person.accomplishment}
+      </p>
+    </li>
+  );
+  return <ul>{listItems}</ul>;
+}
