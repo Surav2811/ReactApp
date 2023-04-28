0e6a42447af95b6f922cb8147778a8bb3057031f
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-04-28 09:50:47 +0530

    Create ExtarctObject.js

---
+++ Tutorial/ExtractObject/ExtarctObject.js
@@ -0,0 +1,26 @@
+const person = {
+  name: 'Gregorio Y. Zara',
+  imageUrl: "https://i.imgur.com/7vQD0fPs.jpg",
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
+export default function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src={person.imageUrl}
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
+}
