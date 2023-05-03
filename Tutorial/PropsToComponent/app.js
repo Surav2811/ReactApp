446a39fb571493efac9a69579de8a34978051bbf
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-05-03 11:13:55 +0530

    Create app.js

---
+++ Tutorial/PropsToComponent/app.js
@@ -0,0 +1,23 @@
+import Avatar from './Avatar.js';
+
+function Card({ children }) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  );
+}
+
+export default function Profile() {
+  return (
+    <Card>
+      <Avatar
+        size={100}
+        person={{ 
+          name: 'Katsuko Saruhashi',
+          imageId: 'YfeOqp2'
+        }}
+      />
+    </Card>
+  );
+}
