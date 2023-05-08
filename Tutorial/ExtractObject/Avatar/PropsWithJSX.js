3c73f1a03e7399c7ec1e487fa2285474c567ae07
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-05-08 09:47:31 +0530

    Create PropsWithJSX.js

---
+++ Tutorial/ExtractObject/Avatar/PropsWithJSX.js
@@ -0,0 +1,12 @@
+function Profile({ person, size, isSepia, thickBorder }) {
+  return (
+    <div className="card">
+      <Avatar
+        person={person}
+        size={size}
+        isSepia={isSepia}
+        thickBorder={thickBorder}
+      />
+    </div>
+  );
+}
