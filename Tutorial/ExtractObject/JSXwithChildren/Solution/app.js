2886b6816a62160e12d8ebe8df7c4dd2c0689276
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-05-11 09:39:57 +0530

    Create app.js

---
+++ Tutorial/ExtractObject/JSXwithChildren/Solution/app.js
@@ -0,0 +1,24 @@
+export default function Profile() {
+  return (
+    <div>
+      <div className="card">
+        <div className="card-content">
+          <h1>Photo</h1>
+          <img
+            className="avatar"
+            src="https://i.imgur.com/OKS67lhm.jpg"
+            alt="Aklilu Lemma"
+            width={70}
+            height={70}
+          />
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-content">
+          <h1>About</h1>
+          <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
