2a630aea4a36e6e7dc95acd954462cd04091db7e
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-05-11 09:38:43 +0530

    Create app.js

---
+++ Tutorial/ExtractObject/AdjustSizeProp/Solution/app.js
@@ -0,0 +1,38 @@
+import { getImageUrl } from './utils.js';
+
+function Avatar({ person, size }) {
+  let thumbnailSize = 's';
+  if (size > 90) {
+    thumbnailSize = 'b';
+  }
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person, thumbnailSize)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+export default function Profile() {
+  return (
+    <>
+      <Avatar
+        size={40}
+        person={{ 
+          name: 'Gregorio Y. Zara', 
+          imageId: '7vQD0fP'
+        }}
+      />
+      <Avatar
+        size={120}
+        person={{ 
+          name: 'Gregorio Y. Zara', 
+          imageId: '7vQD0fP'
+        }}
+      />
+    </>
+  );
+}
