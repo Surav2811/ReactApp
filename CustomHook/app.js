d241d26e60af5a81448df0e18e3155215c2e2cec
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-05-20 14:35:10 +0530

    Create app.js

---
+++ CustomHook/app.js
@@ -0,0 +1,18 @@
+function StatusBar() {
+  const isOnline = useOnlineStatus();
+  return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>;
+}
+
+function SaveButton() {
+  const isOnline = useOnlineStatus();
+
+  function handleSaveClick() {
+    console.log('✅ Progress saved');
+  }
+
+  return (
+    <button disabled={!isOnline} onClick={handleSaveClick}>
+      {isOnline ? 'Save progress' : 'Reconnecting...'}
+    </button>
+  );
+}
