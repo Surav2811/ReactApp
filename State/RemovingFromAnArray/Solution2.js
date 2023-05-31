0ee7f4d265fed3f6a9b964102be0a4e194649ce9
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-05-31 10:47:15 +0530

    Create Solution2.js

---
+++ State/RemovingFromAnArray/Solution2.js
@@ -0,0 +1,53 @@
+//Added both increment and decrement operators
+import { useState } from 'react';
+
+let initialCounters = [
+  0, 0, 0
+];
+
+export default function CounterList() {
+  const [counters, setCounters] = useState(
+    initialCounters
+  );
+
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c + 1;
+      } else {
+        // The rest haven't changed
+        return c;
+      }
+    });
+    setCounters(nextCounters);
+  }
+  function handleDecrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c - 1;
+      } else {
+        // The rest haven't changed
+        return c;
+      }
+    });
+    setCounters(nextCounters);
+  }
+
+  return (
+    <ul>
+      {counters.map((counter, i) => (
+        <li key={i}>
+          {counter}
+          <button onClick={() => {
+            handleIncrementClick(i);
+          }}>+1</button>
+          <button onClick={() => {
+            handleDecrementClick(i);
+          }}>-1</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
