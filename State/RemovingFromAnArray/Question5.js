f7587a1e981b9d8b8f83369c1e4528488fa8b8e9
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2023-06-05 10:54:16 +0530

    Update Question5.js

--- State/RemovingFromAnArray/Question5.js
+++ State/RemovingFromAnArray/Question5.js
@@ -1,4 +1,5 @@
-//
+//This is the Question for Solution 5. And you can replace any of the array values to reflect on the Front-end
+///These are JavaScript codes for a web application that manages a list of artworks to see. The code defines two functions, handleToggleMyList and handleToggleYourList, that handle toggling the "seen" status of an artwork on the user's and another user's list, respectively. The code also defines a ItemList component that renders a list of artworks with checkboxes that allow the user to toggle the "seen" status of each artwork. The ItemList component is used twice in the return statement of a main component that renders the two lists and the h1 and h2 headings.///
 import { useState } from 'react';
 
 let nextId = 3;
